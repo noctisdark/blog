039f054956aae7919ac34bd430be265c6d9bbbe8
noctisdark <majed.abdennadher@protonmail.com>
2022-12-20 16:51:11 +0100

    responsive nav + hero

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    base: true,
+    utils: true,
+    themes: ["light", "dark"]
+  },
+};
