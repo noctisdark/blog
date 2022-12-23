b949d5e4246ca0d5c168fb41aff3f7a0bf9196d0
noctisdark <majed.abdennadher@protonmail.com>
2022-12-23 16:08:41 +0100

    improve color + mode switch

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,22 @@ module.exports = {
     styled: true,
     base: true,
     utils: true,
-    themes: ["light", "dark"],
+    themes: [
+      "light",
+      // "dark"
+      {
+        dark: {
+          primary: "#163D64",
+          secondary: "#62757F",
+          accent: "#DC4318",
+          neutral: "#1D2126",
+          "base-100": "#212121",
+          info: "#0092D6",
+          success: "#6CB288",
+          warning: "#DAAD58",
+          error: "#AB3D30"
+        },
+      },
+    ],
   },
 };
