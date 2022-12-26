73bae677edf6ffb81b6551e8d34e6a542ba68737
noctisdark <majed.abdennadher@protonmail.com>
2022-12-26 14:02:52 +0100

    add Fira Code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: "jit",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      ...defaultTheme.fontFamily,
+      'mono': ['"Fira Code"', ...defaultTheme.fontFamily.mono]
+    },
     extend: {
-      typography: (theme) => ({
+      typography: {
         DEFAULT: {
           css: {
             pre: {
@@ -31,7 +38,7 @@ module.exports = {
             }
           },
         },
-      }),
+      },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
